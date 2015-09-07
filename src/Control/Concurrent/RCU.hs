d35a17299654ffb2424c538089bb17bb77282e21
Edward Kmett <ekmett@gmail.com>
2015-09-07 02:18:59 -0400

    flag these modules as trustworthy

--- src/Control/Concurrent/RCU.hs
+++ src/Control/Concurrent/RCU.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE Trustworthy #-}
 module Control.Concurrent.RCU
   ( SRef
   , RCU
--- src/Control/Concurrent/RCU/Class.hs
+++ src/Control/Concurrent/RCU/Class.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE Trustworthy #-}
 module Control.Concurrent.RCU.Class
   ( SRef
   , MonadNew(..)
