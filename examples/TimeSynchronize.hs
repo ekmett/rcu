3ac277286dd3d8f2b950acc5132ef860f0b2a825
Ted Cooper <anthezium@gmail.com>
2015-10-19 15:28:00 -0700

    Use TSC for benchmarks.
    Run benchmarks in pinned OS threads.
    
    Include version TimeSynchronizeQSBRnoGC that prevents
    pre-emption for GC to reduce measurement variance.

--- examples/TimeSynchronize.hs
+++ examples/TimeSynchronize.hs
@@ -2,19 +2,20 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+import Control.Concurrent
 import Control.Concurrent.RCU.MODE
 import Control.Concurrent.RCU.Class
 import Control.DeepSeq (force)
 import Control.Exception (evaluate)
-import Control.Monad (replicateM, replicateM_)
+import Control.Monad (forM)
 import Data.Bits ((.&.), (.|.), shiftL)
 import Data.List (intercalate)
 import Data.Monoid ((<>))
-import Data.Time (diffUTCTime, getCurrentTime)
 import Data.Word (Word64)
---import Debug.Trace (trace)
 import Options.Applicative (auto, execParser, fullDesc, help, info, long, metavar, option, progDesc, short)
 import Prelude hiding (read)
+import System.CPUTime.Rdtsc
 
 data List s a = Nil | Cons a (SRef s (List s a))
 
@@ -25,10 +26,9 @@ checkForDup :: MonadReading (SRef s) (m s) => (Word64, Bool) -> List s Word64 ->
 checkForDup (_,   dup) Nil         = return dup
 checkForDup (acc, dup) (Cons x rn) = checkForDup (acc .|. x, dup || acc .&. x /= 0) =<< readSRef rn
 
-
-snapshot :: MonadReading (SRef s) (m s) => [a] -> List s a -> m s [a]
-snapshot acc Nil         = return $ reverse acc
-snapshot acc (Cons x rn) = snapshot (x : acc) =<< readSRef rn
+--snapshot :: MonadReading (SRef s) (m s) => [a] -> List s a -> m s [a]
+--snapshot acc Nil         = return $ reverse acc
+--snapshot acc (Cons x rn) = snapshot (x : acc) =<< readSRef rn
 
 reader :: SRef s Bool -> Integer -> SRef s (List s Word64) -> RCU s Integer
 reader rf !acc hd = do
@@ -42,69 +42,90 @@ reader rf !acc hd = do
         then return acc'
         else reader rf acc' hd
 
-moveDback :: SRef s (List s a) -> WritingRCU s ()
-moveDback rl = do
-  Cons _a rb <- readSRef rl
-  Cons _b rc <- readSRef rb
-  -- duplicate pointer to B
-  rb'       <- copySRef rb
-  Cons c rd <- readSRef rc
-  ne        <- readSRef rd
-  -- link in a new C after A
-  writeSRef rb $ Cons c rb'
+writer :: Integer -> WritingRCU s Word64 -> Word64 -> WritingRCU s Word64
+writer 0  _ !acc = return acc
+writer !n m !acc = do d <- m
+                      writer (pred n) m (acc + d)
+                     
+
+moveDback :: SRef s (List s a) -> WritingRCU s Word64
+moveDback rl = WritingRCU $ \ s -> do
+  (rc, ne) <- flip runWritingRCU s $ do
+    Cons _a rb <- readSRef rl
+    Cons _b rc <- readSRef rb
+    -- duplicate pointer to B
+    rb'       <- copySRef rb
+    Cons c rd <- readSRef rc
+    ne        <- readSRef rd
+    -- link in a new C after A
+    writeSRef rb $ Cons c rb'
+    return (rc, ne)
+#if MEASURE_SYNCHRONIZE
+  beg <- rdtsc
+#endif
   -- any reader who starts during this grace period 
   -- sees either "ABCDE" or "ACBCDE"
-  synchronize
+  runWritingRCU synchronize s
+#if MEASURE_SYNCHRONIZE
+  end <- rdtsc
+#else
+  let beg = 0
+      end = 0
+#endif
   -- unlink the old C
-  writeSRef rc ne
+  flip runWritingRCU s $ do writeSRef rc ne
+                            return $ end - beg
 
 testList :: RCU s (SRef s (List s Word64))
 testList = helper 4 =<< newSRef Nil
   where helper (- 1) tl = return tl
         helper i     tl = helper (pred i) =<< newSRef (Cons (shiftL 1 i) tl)
 
-data Opts = Opts { nReaders :: Int
-                 , nUpdates :: Int }
+data Opts = Opts { nReserved :: Integer 
+                 , nUpdates  :: Integer }
 
 main :: IO ()
 main = do 
-  Opts { nReaders, nUpdates } <- execParser opts
-  putStrLn $ "readers: " ++ show nReaders ++ ", updates: " ++ show nUpdates
+  Opts { nReserved, nUpdates } <- execParser opts
+  nCaps <- getNumCapabilities
+  putStrLn $ "nCaps: " ++ show nCaps
+  let nReaders = fromIntegral nCaps - nReserved
+  putStrLn $ "reserved: " ++ show nReserved ++ ", readers: " ++ show nReaders ++ ", updates: " ++ show nUpdates
   let nTotal = fromIntegral $ nUpdates * nReaders :: Double
-  (ods, wd, wfd) <- runRCU $ do
+  (ods, wfrd, wd, wfd) <- runOnRCU 0 $ RCU $ \ s -> do
     -- initialize list
-    hd  <- testList
+    hd  <- unRCU testList s
     -- initialize flag writer uses to stop readers
-    rf  <- newSRef False
+    rf  <- unRCU (newSRef False) s
     -- spawn nReaders readers, each takes snapshots of the list until the writer has finished
-    rts <- replicateM nReaders 
-         $ forking $ reading $ ReadingRCU 
-         $ \ s -> do beg <- getCurrentTime
+    rts <- forM [2..fromIntegral nReaders + 1] $ \ i -> flip unRCU (s { rcuStatePinned = Just i }) $ forking $ reading $ ReadingRCU 
+         $ \ s -> do beg <- rdtsc
                      x   <- evaluate . force =<< unRCU (reader rf 0 hd) s -- how long do the snapshots take?
-                     mid <- getCurrentTime
+                     mid <- rdtsc
                      _   <- evaluate . force $ x -- how long does walking over the result take?
-                     end <- getCurrentTime
+                     end <- rdtsc
                      return ( x
-                            , mid `diffUTCTime` beg
-                            , end `diffUTCTime` mid )
+                            , mid - beg
+                            , end - mid )
     -- spawn a writer to move a node from a later position to an earlier position nUpdates times
-    wt  <- forking $ writing $ WritingRCU 
-         $ \ s -> do beg <- getCurrentTime
-                     x   <- evaluate . force =<< runWritingRCU (replicateM_ nUpdates $ moveDback hd) s
+    wt  <- flip unRCU (s { rcuStatePinned = Just 1 }) $ forking $ writing $ WritingRCU 
+         $ \ s -> do beg <- rdtsc
+                     x   <- evaluate . force =<< runWritingRCU (writer nUpdates (moveDback hd) 0) s
                      runWritingRCU (writeSRef rf True) s
-                     mid <- getCurrentTime
+                     mid <- rdtsc
                      _   <- evaluate . force $ x
-                     end <- getCurrentTime
-                     return ( mid `diffUTCTime` beg
-                            , end `diffUTCTime` mid )
+                     end <- rdtsc
+                     return ( fromIntegral x / fromIntegral nUpdates :: Double
+                            , mid - beg
+                            , end - mid )
     -- wait for the readers to finish
-    ods <- mapM joining rts
+    ods <- mapM (\ rt -> unRCU (joining rt) s) rts
     -- wait for the writer to finish
-    (wd, wfd) <- joining wt
-    return (ods, wd, wfd)
+    (wfrd :: Double, wd, wfd) <- unRCU (joining wt) s
+    return (ods, wfrd, wd, wfd)
   let outs = map a ods
-      rds  = map b ods
-      rfds = map c ods
+      rds  = map b ods :: [Word64]
+      rfds = map c ods :: [Word64]
       a (x,_,_) = x
       b (_,y,_) = y
       c (_,_,z) = z
@@ -114,16 +135,20 @@ main = do
   putStrLn $ "reader evaluate . force times: " ++ show rfds
   putStrLn $ "writer time: " ++ show wd
   putStrLn $ "writer evaluate . force time: " ++ show wfd
-  putStrLn $ "average writer update time: " ++ show ((wd - wfd) / fromIntegral nUpdates)
+  let aud = fromIntegral (wd - wfd) / fromIntegral nUpdates :: Double
+  putStrLn $ "average writer update time: " ++ show aud
+#if MEASURE_SYNCHRONIZE
+  putStrLn $ "average synchronize time: " ++ show wfrd
+#endif
   where opts = info optsParser
              ( fullDesc
             <> progDesc "Measure writer latency for synchronize." )
-        optsParser = Opts <$> nReadersParser <*> nUpdatesParser
-        nReadersParser = option auto
-                       ( long "readers"
-                      <> short 'r'
-                      <> metavar "N"
-                      <> help "Spawn N reader threads" )
+        optsParser = Opts <$> nReservedParser <*> nUpdatesParser
+        nReservedParser = option auto
+                        ( long "reserved"
+                       <> short 'r'
+                       <> metavar "N"
+                       <> help "N OS threads reserved for other stuff" )
         nUpdatesParser = option auto
                        ( long "updates"
                       <> short 'u'
